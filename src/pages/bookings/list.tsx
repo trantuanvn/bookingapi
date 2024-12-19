f33b2dc9fe4c7eafaa11c57801d63b6ff9dc628e
Vien.BIT <Vien.vu@bitelle.com>
2024-12-19 20:38:15 +0700

    12

--- src/pages/bookings/list.tsx
+++ src/pages/bookings/list.tsx
@@ -13,31 +13,53 @@ import { Space, Table } from "antd";
 export const BookingList = () => {
   const { tableProps } = useTable({
     syncWithLocation: true,
+    meta: {
+      populate: ["user", "work_space.space"],
+    },
   });
 
   return (
     <List canCreate={false}>
       <Table {...tableProps} rowKey="id">
-        {/* <Table.Column dataIndex="id" title={"ID"} /> */}
         <Table.Column dataIndex="code" title={"Code"} />
-        <Table.Column dataIndex="title" title={"User"} />
-        <Table.Column dataIndex="title" title={"Space / Workspace"} />
-        <Table.Column dataIndex="title" title={"Date"} />
-        <Table.Column dataIndex="title" title={"Time"} />
-        <Table.Column dataIndex="title" title={"Type"} />
+        <Table.Column
+          dataIndex="user"
+          title={"User"}
+          render={(v) => v?.username}
+        />
+        <Table.Column
+          dataIndex="work_space"
+          title={"Space / Workspace"}
+          render={(r) => `${r?.space?.name} - ${r?.name}`}
+        />
+        <Table.Column
+          dataIndex="start"
+          title={"Date"}
+          render={(r) => <DateField value={r} format="YYYY-MM-DD" />}
+        />
+        <Table.Column
+          dataIndex="start"
+          title={"Time"}
+          render={(r, c: any) => (
+            <p>
+              <DateField value={c.start} format="HH:mm" /> -
+              <DateField value={c.end} format="HH:mm" />
+            </p>
+          )}
+        />
+        <Table.Column dataIndex="type" title={"Type"} />
+        <Table.Column dataIndex="statusCode" title={"Status code"} />
 
         <Table.Column
           title={"Actions"}
           dataIndex="actions"
           render={(_, record: any) => (
             <Space>
-              {/* <EditButton hideText size="small" recordItemId={record.id} /> */}
               <ShowButton
                 hideText
                 size="small"
                 recordItemId={record.documentId}
               />
-              {/* <DeleteButton hideText size="small" recordItemId={record.id} /> */}
             </Space>
           )}
         />
--- src/pages/bookings/show.tsx
+++ src/pages/bookings/show.tsx
@@ -7,42 +7,52 @@ const { Title } = Typography;
 export const BookingShow = () => {
   const { query } = useShow({
     meta: {
-      populate: ["user", "work-space"],
+      populate: ["user", "work_space.space"],
     },
   });
   const { data, isLoading } = query;
 
+  const record = data?.data || {};
+
   return (
     <Show isLoading={isLoading} breadcrumb={false} headerButtons={[]}>
-      <Card title="Booking Details">
-        <Form layout="vertical">
-          <Form.Item label="Code">
-            <Input />
-          </Form.Item>
+      {record?.id && (
+        <Card title="Booking Details">
+          <Form layout="vertical" disabled initialValues={{ ...record }}>
+            <Form.Item label="Code">
+              <Input value={record.code} />
+            </Form.Item>
 
-          <Form.Item label="User">
-            <Input />
-          </Form.Item>
-          <Form.Item label="Space / Workspace">
-            <Input />
-          </Form.Item>
-          <Form.Item label="Date">
-            <Input />
-          </Form.Item>
+            <Form.Item label="User">
+              <Input value={record.user?.username} />
+            </Form.Item>
+            <Form.Item label="Space / Workspace">
+              <Input
+                value={`${record?.work_space?.space.name} /  ${record.work_space.name} `}
+              />
+            </Form.Item>
+            <Form.Item label="Date">
+              <DateField value={record.start} format="DD/MM/YYYY" />
+            </Form.Item>
 
-          <Form.Item label="Time">
-            <Input />
-          </Form.Item>
+            <Form.Item label="Start Time">
+              <DateField value={record.start} format="HH:mm" />
+            </Form.Item>
 
-          <Form.Item label="Type">
-            <Input />
-          </Form.Item>
+            <Form.Item label="end Time">
+              <DateField value={record.end} format="HH:mm" />
+            </Form.Item>
 
-          <Form.Item label="Description">
-            <Input />
-          </Form.Item>
-        </Form>
-      </Card>
+            <Form.Item label="Type">
+              <Input value={record.type} />
+            </Form.Item>
+
+            <Form.Item label="Description">
+              <Input value={record.description} />
+            </Form.Item>
+          </Form>
+        </Card>
+      )}
     </Show>
   );
 };
