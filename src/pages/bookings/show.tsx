125fc57baec69e9299951201c7d6dc91b0da1d70
Vien.BIT <Vien.vu@bitelle.com>
2025-01-17 13:03:05 +0700

    12

--- src/pages/bookings/show.tsx
+++ src/pages/bookings/show.tsx
@@ -1,6 +1,6 @@
 import { DateField, MarkdownField, Show, TextField } from "@refinedev/antd";
 import { useOne, useShow } from "@refinedev/core";
-import { Card, Form, Input, Table, Typography } from "antd";
+import { Card, Col, Form, Input, Row, Table, Typography } from "antd";
 
 const { Title } = Typography;
 
@@ -17,44 +17,48 @@ export const BookingShow = () => {
   return (
     <Show isLoading={isLoading} breadcrumb={false} headerButtons={[]}>
       {record?.id && (
-        <>
-          <Card title="Booking Details">
-            <Form layout="vertical" disabled initialValues={{ ...record }}>
-              <Form.Item label="Code">
-                <Input value={record.code} />
-              </Form.Item>
+        <Row gutter={12}>
+          <Col span={12}>
+            <Card title="Booking Details">
+              <Form layout="vertical" disabled initialValues={{ ...record }}>
+                <Form.Item label="Code">
+                  <Input value={record.code} />
+                </Form.Item>
 
-              <Form.Item label="User">
-                <Input value={record.user?.username} />
-              </Form.Item>
-              <Form.Item label="Type">
-                <Input value={record.type} />
-              </Form.Item>
+                <Form.Item label="User">
+                  <Input value={record.user?.username} />
+                </Form.Item>
+                <Form.Item label="Type">
+                  <Input value={record.type} />
+                </Form.Item>
 
-              <Form.Item label="Description">
-                <Input value={record.description} />
-              </Form.Item>
-            </Form>
-          </Card>
-          <Card title="Booking Details">
-            <Table
-              dataSource={record.booking_items}
-              rowKey="id"
-              pagination={false}
-            >
-              <Table.Column
-                dataIndex="work_space"
-                title={"Space / Workspace"}
-                render={(r) => `${r?.space?.name} - ${r?.name}`}
-              />
-              <Table.Column
-                dataIndex="date"
-                title={"Date"}
-                render={(v, r) => `${v} ${r.start_time}-${r.end_time}`}
-              />
-            </Table>
-          </Card>
-        </>
+                <Form.Item label="Description">
+                  <Input value={record.description} />
+                </Form.Item>
+              </Form>
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card title="Booking Items">
+              <Table
+                dataSource={record.booking_items}
+                rowKey="id"
+                pagination={false}
+              >
+                <Table.Column
+                  dataIndex="work_space"
+                  title={"Space / Workspace"}
+                  render={(r) => `${r?.space?.name} - ${r?.name}`}
+                />
+                <Table.Column
+                  dataIndex="date"
+                  title={"Date"}
+                  render={(v, r) => `${v} ${r.start_time}-${r.end_time}`}
+                />
+              </Table>
+            </Card>
+          </Col>
+        </Row>
       )}
     </Show>
   );
